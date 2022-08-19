b63dc90fac2f24967ec4f2f84d4de0e653d5079c
Amin Meshk <amin.meshk@gmail.com>
2022-08-19 19:46:24 +0430

    Moved todo routes

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,4 @@ Route::get('/', function () {
 Route::get('/dashboard', [TodoController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
-
-Route::get('/todos/create', [TodoController::class, 'create'])->name('todo.create');
-Route::get('/todos/{todo}', [TodoController::class, 'show'])->name('todo.show');
-Route::get('/todos/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todo.update');
-Route::put('/todos/{todo}/done', [TodoController::class, 'done'])->name('todo.done');
-Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
-Route::post('/todos', [TodoController::class, 'store'])->name('todo.store');
+require __DIR__ . '/todo.php';
