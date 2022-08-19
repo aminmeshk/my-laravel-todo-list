77d28d555b72411c42ae3d4727462bb66703e8fd
Amin Meshk <amin.meshk@gmail.com>
2022-08-19 19:09:57 +0430

    Make edit & show buttons responsive

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -10,9 +10,8 @@
       <form action="{{ route('todo.update', ['todo' => $todo->id]) }}" method="POST">
         @csrf
         @method('PUT')
-        <div class="flex flex-row self-stretch justify-end space-x-2 mb-2">
-          <x-button
-            class="space-x-1">
+        <div class="flex flex-col sm:flex-row self-stretch justify-end space-x-2 mb-2">
+          <x-button class="space-x-1 justify-center">
             <span class="material-symbols-outlined text-xl">
               done
             </span>
@@ -21,7 +20,7 @@
             </span>
           </x-button>
           <a href="{{ back()->getTargetUrl() }}"
-            class="inline-flex items-center space-x-1 px-4 py-2 bg-transparent hover:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest active:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+            class="justify-center inline-flex items-center space-x-1 px-4 py-2 bg-transparent hover:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest active:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
             <span class="material-symbols-outlined text-xl">
               close
             </span>
@@ -32,12 +31,18 @@
         </div>
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
-            <input type="text" name="title" id="title" placeholder="Title" value="{{ $todo->title }}"
-              class="flex-1 mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-base font-bold sm:text-lg border-gray-300 rounded-md">
-            {{-- <div class="flex my-1 items-center"> --}}
+            <x-input type="text" name="title" id="title" placeholder="Title"
+              value="{{ old('title') ?? $todo->title }}"
+              class="flex-1 mt-1 block w-full text-base font-bold sm:text-lg" />
+            @error('title')
+              <p class="text-red-700 text-xs m-1">{{ $message }}</p>
+            @enderror
             <textarea
-              class="mt-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-sm sm:text-base border-gray-300 rounded-md"
+              class="mt-2 block w-full text-sm sm:text-base rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
               name="description" rows="10">{{ $todo->description }}</textarea>
+            @error('description')
+              <p class="text-red-700 text-xs m-1">{{ $message }}</p>
+            @enderror
             {{-- </div> --}}
           </div>
         </div>
--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -7,11 +7,11 @@
 
   <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col space-y-2">
-      <div class="flex flex-row self-stretch justify-end space-x-2 mb-2">
+      <div class="flex flex-col sm:flex-row self-stretch justify-end space-x-2 mb-2">
         <form action="{{ route('todo.done', ['todo' => $todo->id]) }}" method="POST">
           @csrf
           @method('PUT')
-          <x-button class="space-x-1" id="done">
+          <x-button class="space-x-1 w-full justify-center" id="done">
             <span class="material-symbols-outlined text-lg">
               done
             </span>
@@ -22,7 +22,7 @@
           <input type="hidden" name="done" value="1" />
         </form>
         <a href="{{ route('todo.edit', ['todo' => $todo]) }}"
-          class="inline-flex items-center space-x-1 px-4 py-2 bg-transparent hover:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest active:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+          class="inline-flex justify-center items-center space-x-1 px-4 py-2 bg-transparent hover:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest active:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
           <span class="material-symbols-outlined text-lg">
             edit
           </span>
@@ -34,7 +34,7 @@
           @csrf
           @method('DELETE')
           <x-button href="{{ route('todo.destroy', ['todo' => $todo]) }}"
-            class="space-x-1 bg-transparent hover:bg-red-100 border-0 text-red-600 active:bg-red-100 focus:bg-red-100 ring-red-200">
+            class="w-full justify-center space-x-1 bg-transparent hover:bg-red-100 border-0 text-red-600 active:bg-red-100 focus:bg-red-100 ring-red-200">
             <span class="material-symbols-outlined text-lg">
               delete
             </span>
@@ -44,7 +44,15 @@
           </x-button>
         </form>
       </div>
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg relative">
+        <div class="bg-green-700 absolute top-0 right-0 -mr-2 -mt-2 rounded-md py-2 px-4 flex flex-row items-center">
+          <span class="material-symbols-outlined text-lg mt-1 mr-1 text-white">
+            done
+          </span>
+          <span class="mt-1 mr-1 text-white">
+            Done
+          </span>
+        </div>
         <div class="p-6 bg-white border-b border-gray-200">
           <div class="flex space-x-2">
             <h2 class="text-lg font-bold flex-1">{{ $todo->title }}</h2>
